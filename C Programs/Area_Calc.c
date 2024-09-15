bed7e6d4a2bc66b374fe7a224e1bf43b10150e12
Vaibhav <vaibhav_sharm@outlook.com>
2024-09-15 22:36:45 +0530

    Added units for each parameter

--- C Programs/Area_Calc.c
+++ C Programs/Area_Calc.c
@@ -12,32 +12,32 @@ int main() {
     double ar_sq, ar_rec, ar_cir;
 
     // Get the side of the square
-    printf("Enter the side of the square: ");
+    printf("Enter the side of the square (units): ");
     scanf("%lf", &a);
 
     // Calculate the area of the square (a^2)
     ar_sq = pow(a, 2);
 
     // Get the length and breadth of the rectangle
-    printf("Enter the length of the rectangle: ");
+    printf("Enter the length of the rectangle (units): ");
     scanf("%lf", &l);
-    printf("Enter the breadth of the rectangle: ");
+    printf("Enter the breadth of the rectangle (units): ");
     scanf("%lf", &b);
 
     // Calculate the area of the rectangle (length * breadth)
     ar_rec = l * b;
 
     // Get the radius of the circle
-    printf("Enter the radius of the circle: ");
+    printf("Enter the radius of the circle (units): ");
     scanf("%lf", &r);
 
     // Calculate the area of the circle (π * r^2)
     ar_cir = M_PI * pow(r, 2);
 
-    // Output the calculated areas
-    printf("The area of the square is: %.2lf\n", ar_sq);
-    printf("The area of the rectangle is: %.2lf\n", ar_rec);
-    printf("The area of the circle is: %.2lf\n", ar_cir);
+    // Output the calculated areas with units of measurement
+    printf("The area of the square is: %.2lf square units\n", ar_sq);
+    printf("The area of the rectangle is: %.2lf square units\n", ar_rec);
+    printf("The area of the circle is: %.2lf square units\n", ar_cir);
 
     return 0;
 }
